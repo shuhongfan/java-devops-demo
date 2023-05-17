2771b3cebe5802aedf062bacad7cf1268ac1e5fd
shuhongfan <shuhongfan@live.com>
2023-05-17 16:23:51 +0800

    first commit

---
+++ src/main/java/com/itdachang/devops/JavaDevopsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.itdachang.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevopsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDevopsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itdachang/devops/JavaDevopsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itdachang.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDevopsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
